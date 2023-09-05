d0768e27765b535f75a665290b35cf815ca78499
Didac19 <diegocastrog19@gmail.com>
2023-09-05 14:26:34 -0500

    Initial commit

---
+++ models/article.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Article.belongsTo(models.User);
+      models.User.hasMany(Article);
+
+      Article.belongsToMany(models.Category, 
+        {through: 'articleCategories', as: 'categories'}
+        );
+
+    }
+  }
+  Article.init({
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'article',
+  },
+  {onDelete: 'RESTRICT', onUpdate: 'CASCADE'});
+  return Article;
+};
